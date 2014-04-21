3a33c7aedd5e7ef4e18f450b07c5206128c167db
Takefumi MIYOSHI <miyo@wasamon.net>
2014-04-21 23:52:41 +0900

    add samples

--- samples/arith.clj
+++ samples/arith.clj
@@ -1,2 +1,12 @@
-(defn add (+ x y))
+(def-module add
+  (fn [x y] (+ x y)))
+
+(def-module sub
+  (fn [x y] (- x y)))
+
+(def-module mac
+  (fn [x y z] (* (+ x y) z)))
+
+(def-module longadd
+  (fn [a b c d e f g] (+ a (+ b (+ c (+ d (+ e (+ f g))))))))
 
